7ee5fe00dc0c82667f07954846683042c12111df
Timothy Wang <timothywang@microsoft.com>
2024-05-13 13:49:11 -0400

    Push new changes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        Hello new change. Hello world!
+        Hello new change. Hello world! Howdy!
       </p>
     </>
   )
