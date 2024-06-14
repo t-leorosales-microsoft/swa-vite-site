fbd755a7a46e00f4229e7d196e8d28421a39630a
Timothy Wang <timothywang@microsoft.com>
2024-06-14 08:55:32 -0700

    Push change to force rebuild

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        Hello new change. Hello world! Howdy 🤠!
+        Hello new change. Hello world! Howdy 🤠! Yippe!
       </p>
     </>
   )
